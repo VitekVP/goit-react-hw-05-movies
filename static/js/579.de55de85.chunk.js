c7e9dd5378b7e430a8683316b9e7f7c9bd24935a
VitekVP <114882925+VitekVP@users.noreply.github.com>
2023-05-31 17:51:35 +0000

    Deploying to gh-pages from @ VitekVP/goit-react-hw-05-movies@31025d71be29cec835ce6c6c912f83483dd54a17 🚀

---
+++ static/js/579.de55de85.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[579],{9579:function(e,t,r){r.r(t);var a=r(7689),c=r(184);t.default=function(){return(0,c.jsx)(a.Fg,{to:"/",replace:!0})}}}]);
+//# sourceMappingURL=579.de55de85.chunk.js.map
